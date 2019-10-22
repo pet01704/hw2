04300449d47774fc5e58393ae9ce3e6d37f2046e
Daniel Peterson <pet01704@umn.edu>
2019-10-22 15:43:53 -0500

    comments in phase2.c

--- Template/src/phase2.c
+++ Template/src/phase2.c
@@ -1,9 +1,60 @@
 #include "phase2.h"
-// You are free to use your own logic. The following points are just for getting started
-/* 	Map Function
-	1)	Each mapper selects a Mapper_i.txt to work with
-	2)	Creates a list of letter, wordcount from the text files found in the Mapper_i.txt
-	3)	Send the list to Reducer via pipes with proper closing of ends
-*/
+//Phase 2
+/*
 
-//git test
+void master(){
+
+	//open the directory /MapperInput
+	//get number of files in /MapperInput, n
+
+
+	//create pipe
+
+	//start child process
+	//fork()
+	//if child
+		//start the reducer
+		//run reduce(pipe)		
+		return;
+
+	//if parent
+		//continue
+
+
+	for (int i = 0; i < n; i++){
+		
+		
+
+		//Create a pipe
+		// map(i) -> reduce
+		//pass output to reduce		
+
+		//start child process
+		//fork()
+		//if child
+			//run map(i,pipe)
+			return;
+		//if parent
+			//continue
+	}	
+	return;
+}		
+
+void map(int i){
+	//open Mapper_i.txt from /MapperInput
+		
+	//find the number of words corresponding to each letter of the alphabet from the file
+	//case insensitive
+
+        //write the list to sdout
+	return;
+}
+
+
+
+No output
+
+The grading will be carried out based on the per letter word
+count algorithm, pipe setup and usage.
+
+/*
